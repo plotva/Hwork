b130c5ee2402ea581491ce267caba7d123b8dc5d
Aleksandr Denisov <Aleksandr_Denisov1@epam.com>
2023-07-05 18:05:55 +0400

    Add comment in Hw2_part1

--- Hw2_part1.py
+++ Hw2_part1.py
@@ -2,58 +2,63 @@ import requests
 import sqlite3
 from bs4 import BeautifulSoup
 import time
-page_c = []
-count = 0
+import asyncio
+user_agent = {'User-agent': 'Mozilla/5.0'}
+#Функция для получения ваканции по id
+async def get_vacancy_by_id(id, user_agent):
+    url = f'https://hh.ru/vacancy/{id}'  
+    vacancy = requests.get(url,headers = user_agent)
+    soup = BeautifulSoup(vacancy.content.decode(), 'lxml')
+    asyncio.timeout(10)
+    return soup
+#Функция для запуска запросов для получения вакансий и их парсинга
+async def main(ids):
+    tasks = []
+    data = []
+    #Создаём задания для запуска для получения страниц вакансий по их id
+    for id in ids:
+        tasks.append(asyncio.create_task(get_vacancy_by_id(id, user_agent)))
+    results = await asyncio.gather(*tasks)
+    #Парсим результат
+    for soup in results:
+        key_skills = ""
+        name = soup.find('h1')
+        company_name = soup.find('a', attrs={'data-qa': 'vacancy-company-name'})
+        if not company_name:
+         company_name = soup.find('div', attrs={'data-qa': 'vacancy-company__details'})
+         if not company_name :
+            company_name = "Вакансия в архиве"
+        job_description = soup.find('div', attrs={'data-qa': 'vacancy-description'})
+        if not job_description:
+            job_description = "Нет описания"
+        else : job_description=job_description.text
+        skills = soup.find_all('div', attrs={'data-qa': 'bloko-tag bloko-tag_inline skills-element'})
+        for skil in skills :
+            key_skills=key_skills  + skil.text + ","
+        data.append((company_name.text,name.text,job_description,key_skills[:-1]))             
+    #Добавляем в базу
+    cursor.executemany("insert into vacancies(company_name,position,job_description,key_skills) values (?,?,?,?)",data)    
+    sqlite_connection.commit()
 try:
     sqlite_connection = sqlite3.connect('./Hwork/hw2.db')
     cursor = sqlite_connection.cursor()
-    #create table
+    #Создаём таблицу
     cursor.execute("DROP TABLE IF EXISTS vacancies;")
     cursor.execute("CREATE TABLE IF NOT EXISTS vacancies (id integer primary key, company_name text, position text, job_description text, key_skills text);")
-    url_main = ' https://hh.ru/search/vacancy?text=middle python developer&salary=&ored_clusters=true&search_field=name&search_field=description'
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    result = requests.get(url_main, headers=user_agent)
-    result.content.decode()
-    soup = BeautifulSoup(result.content.decode(), 'lxml')
-    papers = soup.find_all('a', attrs={'data-qa': 'pager-page'})
-    for paper in papers :
-        page_c.append(paper.text)
-    for index in range (1, int(page_c[-1]) + 1 ) :
-        url=f"https://hh.ru//search/vacancy?text=middle python developer&amp;salary=&amp;ored_clusters=true&amp;search_field=name&amp;search_field=description&amp;page=&amp;hhtmFrom=vacancy_search_list&page={index}"
-        data = []
+    #Получаем страницу со списком вакансий, по 20 штук на страницу и проходим 5 страниц
+    for index in range (0, 5) :
+        url=f"https://hh.ru//search/vacancy?text=middle python developer&page={index}&salary=&ored_clusters=true&search_field=name&search_field=name&items_on_page=20"
+        #Парсим из url id каждой вакансии
+        ids = []
         result = requests.get(url, headers=user_agent)
         result.content.decode()
         soup = BeautifulSoup(result.content.decode(), 'lxml')
-        names = soup.find_all('a', attrs={'data-qa': 'serp-item__title'})
-        for name in names:
-            id=name.attrs.get('href')
-            print(id.split('?')[0].split('/')[-1])
-            key_skills = ""
-            nn = name.text.lower()
-            if "python" in nn :
-                result_v = requests.get(name.attrs.get('href'), headers=user_agent) 
-                result_v.content.decode()
-                soup_v = BeautifulSoup(result_v.content.decode(), 'lxml') 
-                company_name = soup_v.find('a', attrs={'data-qa': 'vacancy-company-name'})
-                if not company_name:
-                    company_name = soup_v.find('div', attrs={'data-qa': 'vacancy-company__details'})
-                    if not company_name :
-                            flag_archive = True
-                job_description = soup_v.find('div', attrs={'data-qa': 'vacancy-description'})
-                skills = soup_v.find_all('div', attrs={'data-qa': 'bloko-tag bloko-tag_inline skills-element'})
-                for skil in skills :
-                    key_skills=key_skills  + skil.text + ", "
-                if count < 100 :
-                    if not flag_archive :
-                        data.append((company_name.text,name.text,job_description.text,key_skills[:-1]))
-                        count += 1
-                    #print (data)
-                    
-                else : pass
-                time.sleep(1)
-        cursor.executemany("insert into vacancies(company_name,position,job_description,key_skills) values (?,?,?,?)",data)    
-        sqlite_connection.commit()
-       
+        vac = soup.find_all('a', attrs={'data-qa': 'serp-item__title'})
+        for id_vac in vac:
+           ids.append(id_vac.attrs.get('href').split('?')[0].split('/')[-1])
+        asyncio.run(main(ids))
+        #antiban in hh
+        time.sleep(0.5)     
 except sqlite3.Error as error:
     print("Error in sqlite :: ", error)
 finally:
